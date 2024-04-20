7d2c243eef3958ae654b35b74968aa0c0e96fda8
王冠升 <wangguansheng@wangguanshengs-MacBook-Air.local>
2024-04-20 17:03:49 +0800

    feat: version 0.01

---
+++ bot.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+)
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := bot.ParseRequest(r)
+
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(http.StatusBadRequest)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			// Handle only on text message
+			case *linebot.TextMessage:
+				if !isGroupEvent(event) {
+					return
+				}
+
+				if strings.HasPrefix(message.Text, "帥狗畫") {
+					handleDraw(event, message.Text)
+				} else if strings.EqualFold(message.Text, "帥狗總結一下") {
+					handleSumAll(event)
+				} else if strings.HasPrefix(message.Text, "帥狗 ") {
+					handleReply(event, message.Text)
+				} else {
+					handleStoreMsg(event, message.Text)
+				}
+
+			// Handle only on Sticker message
+			case *linebot.StickerMessage:
+				var kw string
+				for _, k := range message.Keywords {
+					kw = kw + "," + k
+				}
+
+				log.Println("Sticker: PID=", message.PackageID, " SID=", message.StickerID, "keyword:", kw, "text:", message.Text)
+
+				if isGroupEvent(event) {
+					outStickerResult := fmt.Sprintf("貼圖訊息: %s ", kw)
+					handleStoreMsg(event, outStickerResult)
+				}
+			}
+		}
+	}
+}
+
+func handleSumAll(event *linebot.Event) {
+	// Scroll through all the messages in the chat group (in chronological order).
+	oriContext := ""
+	q := summaryQueue.ReadGroupInfo(getGroupID(event))
+	for _, m := range q {
+		// [xxx]: ...
+		userName, _ := bot.GetProfile(m.UserName).Do()
+		oriContext = oriContext + fmt.Sprintf("[%s]: %s . %s\n", userName, m.MsgText, m.Time.Local().UTC().Format("2006-01-02 15:04:05"))
+	}
+
+	userName := event.Source.UserID
+	userProfile, err := bot.GetProfile(event.Source.UserID).Do()
+	if err == nil {
+		userName = userProfile.DisplayName
+	}
+
+	if oriContext == "" {
+		if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("沒事情總結了Ｒ "+userName)).Do(); err != nil {
+			log.Print(err)
+		}
+		return
+	}
+
+	oriContext = fmt.Sprintf("幫我總結成三點 `%s`", oriContext)
+	reply := gptGPT3CompleteContext(oriContext)
+	log.Println(oriContext)
+
+	if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(reply)).Do(); err != nil {
+		log.Print(err)
+	}
+}
+
+func handleReply(event *linebot.Event, askStr string) {
+	// Scroll through all the messages in the chat group (in chronological order).
+	oriContext := ""
+	q := summaryQueue.ReadGroupInfo(getGroupID(event))
+	for _, m := range q {
+		// [xxx]: ...
+		oriContext = oriContext + fmt.Sprintf("[%s]: %s . %s\n", m.UserName, m.MsgText, m.Time.Local().UTC().Format("2006-01-02 15:04:05"))
+	}
+
+	userName := event.Source.UserID
+	userProfile, err := bot.GetProfile(event.Source.UserID).Do()
+	if err == nil {
+		userName = userProfile.DisplayName
+	}
+
+	askStr = strings.TrimPrefix(askStr, "帥狗 ")
+	log.Print(askStr)
+
+	// prompt
+	oriContext = fmt.Sprintf("你是隻有著帥氣假髮的哈吧狗，大家都叫你帥狗，我是`%s`, 我想跟你說：`%s`,你在一個聊天的群組裡，請在群組中回覆我一些話，簡短並帶點幽默，語氣可以隨便一點，控制在兩句內，這是這個群組目前的對話內容：`%s`，你是群組中的帥狗，直接回覆你要說的就好不用加上自己的名稱", userName, askStr, oriContext)
+	reply := gptGPT3CompleteContext(oriContext)
+	log.Print(oriContext)
+
+	m := MsgDetail{
+		MsgText:  reply,
+		UserName: "帥狗",
+		Time:     time.Now(),
+	}
+
+	summaryQueue.AppendGroupInfo(getGroupID(event), m)
+
+	if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(reply)).Do(); err != nil {
+		log.Print(err)
+	}
+}
+
+func handleDraw(event *linebot.Event, message string) {
+	message = strings.TrimPrefix(message, "帥狗畫")
+
+	if reply, err := gptImageCreate(message); err != nil {
+		if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("帥狗畫不出來")).Do(); err != nil {
+			log.Print(err)
+		}
+	} else {
+		if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("帥狗畫好啦："), linebot.NewImageMessage(reply, reply)).Do(); err != nil {
+			log.Print(err)
+		}
+	}
+}
+
+func handleStoreMsg(event *linebot.Event, message string) {
+	// Get user display name. (It is nick name of the user define.)
+	userName := event.Source.UserID
+	userProfile, err := bot.GetProfile(event.Source.UserID).Do()
+	if err == nil {
+		userName = userProfile.DisplayName
+	}
+
+	m := MsgDetail{
+		MsgText:  message,
+		UserName: userName,
+		Time:     time.Now(),
+	}
+	summaryQueue.AppendGroupInfo(getGroupID(event), m)
+}
+
+func isGroupEvent(event *linebot.Event) bool {
+	return event.Source.GroupID != "" || event.Source.RoomID != ""
+}
+
+func getGroupID(event *linebot.Event) string {
+	if event.Source.GroupID != "" {
+		return event.Source.GroupID
+	} else if event.Source.RoomID != "" {
+		return event.Source.RoomID
+	}
+
+	return ""
+}
---
+++ chatgpt.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/sashabaranov/go-openai"
+)
+
+func gptGPT3CompleteContext(content string) (ret string) {
+	fmt.Printf("Using GPT3\n")
+	return gptCompleteContext(content, openai.GPT3Dot5Turbo)
+}
+
+func gptCompleteContext(content string, model string) (ret string) {
+	ctx := context.Background()
+
+	// See: https://platform.openai.com/docs/guides/chat
+	//			https://github.com/sashabaranov/go-openai
+	req := openai.ChatCompletionRequest{
+		Model: model,
+		Messages: []openai.ChatCompletionMessage{{
+			Role:    openai.ChatMessageRoleUser,
+			Content: content,
+		}},
+	}
+
+	resp, err := client.CreateChatCompletion(ctx, req)
+	if err != nil {
+		ret = fmt.Sprintf("Err: %v", err)
+	} else {
+		// The response contains a list of choices, each with a score.
+		// The score is a float between 0 and 1, with 1 being the most likely.
+		// The choices are sorted by score, with the first choice being the most likely.
+		// So we just take the first choice.
+		ret = resp.Choices[0].Message.Content
+	}
+
+	return ret
+}
+
+// Create image by DALL-E 2
+func gptImageCreate(prompt string) (string, error) {
+	ctx := context.Background()
+
+	reqUrl := openai.ImageRequest{
+		Prompt:         prompt,
+		Size:           openai.CreateImageSize256x256,
+		ResponseFormat: openai.CreateImageResponseFormatURL,
+		N:              1,
+	}
+
+	respUrl, err := client.CreateImage(ctx, reqUrl)
+	if err != nil {
+		fmt.Printf("Image creation error: %v\n", err)
+		return "", errors.New("image creation error")
+	}
+	fmt.Println(respUrl.Data[0].URL)
+
+	return respUrl.Data[0].URL, nil
+}
